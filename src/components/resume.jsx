8fd3432b8329c1d59e94975d1ec5bbaa46c02a17
Rui Pinge <rui@pinge.org>
2019-03-19 00:29:58 +0000

    Course name update.

--- src/components/resume.jsx
+++ src/components/resume.jsx
@@ -246,7 +246,7 @@ export default class Resume extends Component {
                 <LocationName>Lisbon</LocationName>
               </Location>
             </Company>
-            <Role>BSc Computer and Software Engineering<DateRange>| &nbsp;Sep '07 - Jul '03</DateRange></Role>
+            <Role>MSc in Computer Science and Engineering<DateRange>| &nbsp;Sep '07 - Jul '03</DateRange></Role>
           </Period>
 
           <br />
